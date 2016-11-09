2952edcf7116d88b1fa912e20cea5e36881a1601
gatchi <christen.got@gmail.com>
2016-11-08 16:01:15 -0800

    init commit

---
+++ src/main/java/com/gatchi/tumblr_agent/DashActivity.java
@@ -0,0 +1,14 @@
+package com.gatchi.tumblr_agent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DashActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+	}
+}
