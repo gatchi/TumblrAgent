cb158c3858271b85116a8c6c730582af4ae4aa7f
gatchi <christen.got@gmail.com>
2016-11-10 18:28:30 -0800

    added util class featuring debug tag
    for Log

---
+++ src/main/java/com/gatchi/tumblr_agent/Util.java
@@ -0,0 +1,11 @@
+package com.gatchi.tumblr_agent;
+
+/**
+ * Class for miscellaneous features.
+ */
+public class Util {
+
+	/** Tag used for debugging. */
+	public static final String TAG = "TumblrAgent";
+	
+}
